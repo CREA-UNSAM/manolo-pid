2fcd5519c199f0bc036ceca62d4f6f0ea25e06d5
Matias Picon <matiasgpicon@gmail.com>
2024-08-02 23:58:37 -0300

    incorporate tomi code

--- manolo-pid.ino
+++ manolo-pid.ino
@@ -5,20 +5,19 @@
 // #define MAXOUTPUT 255
 #define SPEED_BASE 80
 
-
 // PIN DEFINITIONS
-const int PIN_LED = 8;           //D8 | Digital 8 | GPIO 14 
-const int PIN_BUTTON = 9;        //D9 | Digital 9 | GPIO 15
+const int PIN_LED = 8;    // D8 | Digital 8 | GPIO 14
+const int PIN_BUTTON = 9; // D9 | Digital 9 | GPIO 15
 
 // MOTOR RIGHT
-const int PIN_MOTOR_R_PWM = 5;   //D5 | Digital 5 | GPIO 11
-const int PIN_MOTOR_R_1 = 7;     //D6 | Digital 6 | GPIO 12
-const int PIN_MOTOR_R_2 = 6;     //D7 | Digital 7 | GPIO 13
+const int PIN_MOTOR_R_PWM = 5; // D5 | Digital 5 | GPIO 11
+const int PIN_MOTOR_R_1 = 7;   // D6 | Digital 6 | GPIO 12
+const int PIN_MOTOR_R_2 = 6;   // D7 | Digital 7 | GPIO 13
 
 // MOTOR LEFT
-const int PIN_MOTOR_L_PWM = 3;    //D3 | Digital 3 | GPIO 5
-const int PIN_MOTOR_L_1 = 4;      //D2 | Digital 2 | GPIO 4
-const int PIN_MOTOR_L_2 = 2;      //D4 | Digital 4 | GPIO 6
+const int PIN_MOTOR_L_PWM = 3; // D3 | Digital 3 | GPIO 5
+const int PIN_MOTOR_L_1 = 4;   // D2 | Digital 2 | GPIO 4
+const int PIN_MOTOR_L_2 = 2;   // D4 | Digital 4 | GPIO 6
 
 // SENSOR PINS
 const int PIN_SENSOR_0 = 11;
@@ -56,7 +55,19 @@ float correction = 0.0;
 int left_motor_speed = 0.0;
 int right_motor_speed = 0.0;
 
-void setup() {
+const unsigned int blinkDelay = 490;
+const unsigned int runDelay = 0;
+bool state = false;
+bool blink = true;
+bool button_state;
+
+unsigned long blinkTimer;
+unsigned long runTimer;
+
+int memory = 0;
+
+void setup()
+{
   // Initialize serial communication
   Serial.begin(9600);
   Serial.println(" ----------------------------------------------------Start------------------------------------------------------------------ ");
@@ -74,8 +85,6 @@ void setup() {
   Serial.println(" ----------------------------------------------------Start------------------------------------------------------------------ ");
   Serial.println(" ----------------------------------------------------Start------------------------------------------------------------------ ");
   Serial.println(" ----------------------------------------------------Start------------------------------------------------------------------ ");
-          
-          
 
   // Initialize the LED pin as an output
   pinMode(PIN_LED, OUTPUT);
@@ -84,12 +93,14 @@ void setup() {
   pinMode(PIN_BUTTON, INPUT_PULLUP);
 
   // Initialize 6 analog inputs for sensors
-  for (int i = 0; i < CANT_ANALOG_SENSORS; i++) {
+  for (int i = 0; i < CANT_ANALOG_SENSORS; i++)
+  {
     pinMode(PINS_ANALOG_SENSORS[i], INPUT);
   }
 
   // Initialize 2 digital inputs for sensors
-  for (int i = 0; i < CANT_DIGITAL_SENSORS; i++) {
+  for (int i = 0; i < CANT_DIGITAL_SENSORS; i++)
+  {
     pinMode(PINS_DIGITAL_SENSORS[i], INPUT);
   }
 
@@ -101,20 +112,176 @@ void setup() {
   pinMode(PIN_MOTOR_R_PWM, OUTPUT);
   pinMode(PIN_MOTOR_R_1, OUTPUT);
   pinMode(PIN_MOTOR_R_2, OUTPUT);
-
 }
-
-
 
 void run(unsigned long currentTime)
 {
-  if((currentTime - runTimer) >= runDelay)
+  if ((currentTime - runTimer) >= runDelay)
   {
-    readSensorData();
-  
-    handleMotorSpeed();
+    for (int i = 0; i < CANT_ANALOG_SENSORS; i++)
+    {
+      analogSensorValues[i] = analogRead(PINS_ANALOG_SENSORS[i]);
 
-    printSerialData();
+      // Invert the reading if the line is white on black, 1 is black line
+      if (LOGIC != 1)
+      {
+        analogSensorValues[i] = 1023 - analogSensorValues[i];
+      }
+    }
+
+    sensorValues[0] = !digitalRead(PINS_DIGITAL_SENSORS[0]);
+
+    //----analog to digital conversion
+    for (int i = 0; i < CANT_ANALOG_SENSORS; i++)
+    {
+      sensorValues[i + 1] = analogSensorValues[i] > 512 ? 0 : 1;
+    }
+
+    sensorValues[CANT_ALL_SENSORS - 1] = !digitalRead(PINS_DIGITAL_SENSORS[1]);
+
+    int weights[] = {5, 3, 2, 1, 1, 2, 3, 5};
+
+    int maxLeftDetections = 0;
+    for (int i = 0; i < CANT_ALL_SENSORS / 2; i++)
+    {
+      if (sensorValues[i] == 1 && weights[i] > maxLeftDetections)
+      {
+        maxLeftDetections = weights[i];
+      }
+    }
+    Serial.print(maxLeftDetections);
+    int maxRightDetections = 0;
+    for (int i = CANT_ALL_SENSORS / 2; i < CANT_ALL_SENSORS; i++)
+    {
+      if (sensorValues[i] == 1 && weights[i] > maxRightDetections)
+      {
+        maxRightDetections = weights[i];
+      }
+    }
+    Serial.print(maxRightDetections);
+
+    // Calculate the weighted sum of sensor values
+    Input = (maxLeftDetections * 10) - (maxRightDetections * 10);
+
+    // Compute PID output
+    float position = Input;                                          // Lee la posición de la línea
+    error = setpoint - position;                                     // Calcula el error
+    integral += error;                                               // Acumula el error para el término integral
+    derivative = error - previous_error;                             // Calcula el cambio en el error
+    correction = (KP * error) + (KI * integral) + (KD * derivative); // Calcula la corrección
+    previous_error = error;                                          // Actualiza el error previo
+
+    Output = correction;
+
+    motorspeedL = constrain(SPEED_BASE + correction, MOTORS_MIN_PWM_VALUE, MOTORS_MAX_PWM_VALUE);
+    motorspeedR = constrain((SPEED_BASE - correction) - 20, MOTORS_MIN_PWM_VALUE, MOTORS_MAX_PWM_VALUE - 20);
+
+
+    motorspeedL = SPEED_BASE;
+    motorspeedR = 
+
+    // Motor izquierdo
+    if (motorspeedL > 0)
+    {
+      digitalWrite(PIN_MOTOR_L_1, HIGH);
+      digitalWrite(PIN_MOTOR_L_2, LOW);
+      analogWrite(PIN_MOTOR_L_PWM, motorspeedL);
+    }
+    else if (motorspeedL < 0)
+    {
+      digitalWrite(PIN_MOTOR_L_1, LOW);
+      digitalWrite(PIN_MOTOR_L_2, HIGH);
+      analogWrite(PIN_MOTOR_L_PWM, -motorspeedL); // multiplica por -1 porque el valor es negativo
+    }
+    else
+    {
+      digitalWrite(PIN_MOTOR_L_1, LOW);
+      digitalWrite(PIN_MOTOR_L_2, LOW);
+      analogWrite(PIN_MOTOR_L_PWM, 0);
+    }
+
+    // Motor derecho
+    if (motorspeedR > 0)
+    {
+      digitalWrite(PIN_MOTOR_R_1, HIGH);
+      digitalWrite(PIN_MOTOR_R_2, LOW);
+      analogWrite(PIN_MOTOR_R_PWM, motorspeedR);
+    }
+    else if (motorspeedR < 0)
+    {
+      digitalWrite(PIN_MOTOR_R_1, LOW);
+      digitalWrite(PIN_MOTOR_R_2, HIGH);
+      analogWrite(PIN_MOTOR_R_PWM, -motorspeedR);
+    }
+    else
+    {
+      digitalWrite(PIN_MOTOR_R_1, LOW);
+      digitalWrite(PIN_MOTOR_R_2, LOW);
+      analogWrite(PIN_MOTOR_R_PWM, 0);
+    }
+
+    Serial.print(" | ");
+
+    // Monitorización por serial
+    Serial.print("SA: ");
+    for (int i = 0; i < CANT_ANALOG_SENSORS; i++)
+    {
+      Serial.print(analogSensorValues[i]);
+      Serial.print(" ");
+    }
+    Serial.print(" | ");
+    for (int i = 0; i < CANT_ALL_SENSORS; i++)
+    {
+      Serial.print(sensorValues[i]);
+      Serial.print(" ");
+    }
+
+    Serial.print(" | Input: ");
+    Serial.print(Input);
+    Serial.print(" | Output: ");
+    Serial.print(Output);
+    Serial.print(" | Motor Speed R: ");
+    Serial.print(motorspeedL);
+    Serial.print(" | Motor Speed L: ");
+    Serial.println(motorspeedR);
+    Serial.print(" | Kp: ");
+    Serial.print(KP, 2);
+    Serial.print(" | Ki: ");
+    Serial.print(KI, 2);
+    Serial.print(" | Kd: ");
+    Serial.print(KD, 2);
+    Serial.print(" | ");
+
+    // Check for serial input
+    if (Serial.available())
+    {
+      String input = Serial.readStringUntil('\n');
+      input.trim();
+
+      // Parse the input and update PID constants
+      if (input.startsWith("Kp"))
+      {
+        KP = input.substring(3).toFloat();
+
+        Serial.print("Kp updated to: ");
+        Serial.println(KP);
+      }
+      else if (input.startsWith("Ki"))
+      {
+        KI = input.substring(3).toFloat();
+
+        Serial.print("Ki updated to: ");
+        Serial.println(KI);
+      }
+      else if (input.startsWith("Kd"))
+      {
+        KD = input.substring(3).toFloat();
+        Serial.print("Kd updated to: ");
+        Serial.println(KD);
+      }
+    }
+
+    delay(1); // Ajusta el retardo según sea necesario
 
     runTimer = currentTime;
     digitalWrite(PIN_LED, HIGH);
@@ -123,7 +290,7 @@ void run(unsigned long currentTime)
 
 void stop(unsigned int currentTime)
 {
-  if((currentTime - blinkTimer) >= blinkDelay)
+  if ((currentTime - blinkTimer) >= blinkDelay)
   {
     analogWrite(PIN_MOTOR_L_PWM, 0);
     analogWrite(PIN_MOTOR_R_PWM, 0);
@@ -134,9 +301,10 @@ void stop(unsigned int currentTime)
   }
 }
 
-void loop() {
+void loop()
+{
   unsigned long currentTime = millis();
-  if(state)
+  if (state)
   {
     run(currentTime);
   }
@@ -145,7 +313,7 @@ void loop() {
     stop(currentTime);
   }
 
-  if(button_state && !digitalRead(PIN_BUTTON))
+  if (button_state && !digitalRead(PIN_BUTTON))
   {
     state = !state;
     Serial.println(state ? "RUN" : "STOP");
@@ -153,147 +321,4 @@ void loop() {
 
   button_state = digitalRead(PIN_BUTTON);
   delay(1);
-}
-void run() {
-
-
-  for (int i = 0; i < CANT_ANALOG_SENSORS; i++) {
-    analogSensorValues[i] = analogRead(PINS_ANALOG_SENSORS[i]);
-
-
-  // Invert the reading if the line is white on black, 1 is black line
-    if (LOGIC != 1) {
-      analogSensorValues[i] = 1023 - analogSensorValues[i];
-    }
-  }
-
-  sensorValues[0] = !digitalRead(PINS_DIGITAL_SENSORS[0]);
-
-  //----analog to digital conversion
-  for (int i = 0; i < CANT_ANALOG_SENSORS; i++) {
-    sensorValues[i + 1] = analogSensorValues[i] > 512 ? 0 : 1;
-    }
-
-  sensorValues[CANT_ALL_SENSORS - 1] = !digitalRead(PINS_DIGITAL_SENSORS[1]);
-
-  int weights[] = {5, 3, 2, 1, 1, 2, 3, 5};
-  
-  int maxLeftDetections = 0;
-  for (int i = 0; i < CANT_ALL_SENSORS / 2; i++) {
-    if (sensorValues[i] == 1 && weights[i] > maxLeftDetections){
-      maxLeftDetections = weights[i];
-    }
-  }
-  Serial.print(maxLeftDetections);
-  int maxRightDetections = 0;
-  for (int i = CANT_ALL_SENSORS / 2; i < CANT_ALL_SENSORS; i++) {
-    if (sensorValues[i] == 1 && weights[i] > maxRightDetections){
-      maxRightDetections = weights[i];
-    }
-  }
-  Serial.print(maxRightDetections);
-
-
-  // Calculate the weighted sum of sensor values
-  Input = (maxLeftDetections * 10) - (maxRightDetections * 10);
-
-  // Compute PID output
-  float position = Input; // Lee la posición de la línea
-  error = setpoint - position; // Calcula el error
-  integral += error; // Acumula el error para el término integral
-  derivative = error - previous_error; // Calcula el cambio en el error
-  correction = (KP * error) + (KI * integral) + (KD * derivative); // Calcula la corrección
-  previous_error = error; // Actualiza el error previo
-
-  Output = correction;
-
-
-  motorspeedL = constrain(SPEED_BASE + correction, MOTORS_MIN_PWM_VALUE, MOTORS_MAX_PWM_VALUE);
-  motorspeedR = constrain((SPEED_BASE - correction)- 20, MOTORS_MIN_PWM_VALUE, MOTORS_MAX_PWM_VALUE-20);
-
-  // Motor izquierdo
-  if (motorspeedL > 0) {
-    digitalWrite(PIN_MOTOR_L_1, HIGH);
-    digitalWrite(PIN_MOTOR_L_2, LOW);
-    analogWrite(PIN_MOTOR_L_PWM, motorspeedL);
-  } else if (motorspeedL < 0) {
-    digitalWrite(PIN_MOTOR_L_1, LOW);
-    digitalWrite(PIN_MOTOR_L_2, HIGH);
-    analogWrite(PIN_MOTOR_L_PWM, -motorspeedL);//multiplica por -1 porque el valor es negativo
-  } else {
-    digitalWrite(PIN_MOTOR_L_1, LOW);
-    digitalWrite(PIN_MOTOR_L_2, LOW);
-    analogWrite(PIN_MOTOR_L_PWM, 0);
-  }
-
-  // Motor derecho
-  if (motorspeedR > 0) {
-    digitalWrite(PIN_MOTOR_R_1, HIGH);
-    digitalWrite(PIN_MOTOR_R_2, LOW);
-    analogWrite(PIN_MOTOR_R_PWM, motorspeedR);
-  } else if (motorspeedR < 0) {
-    digitalWrite(PIN_MOTOR_R_1, LOW);
-    digitalWrite(PIN_MOTOR_R_2, HIGH);
-    analogWrite(PIN_MOTOR_R_PWM, -motorspeedR); 
-  } else {
-    digitalWrite(PIN_MOTOR_R_1, LOW);
-    digitalWrite(PIN_MOTOR_R_2, LOW);
-    analogWrite(PIN_MOTOR_R_PWM, 0);
-  }
-
-  Serial.print(" | ");
-
-  // Monitorización por serial
-  Serial.print("SA: ");
-  for (int i = 0; i < CANT_ANALOG_SENSORS; i++) {
-    Serial.print(analogSensorValues[i]);
-    Serial.print(" ");
-  }
-  Serial.print(" | ");
-  for (int i = 0; i < CANT_ALL_SENSORS; i++) {
-    Serial.print(sensorValues[i]);
-    Serial.print(" ");
-  }
-
-  Serial.print(" | Input: ");
-  Serial.print(Input);
-  Serial.print(" | Output: ");
-  Serial.print(Output);
-  Serial.print(" | Motor Speed R: ");
-  Serial.print(motorspeedL);
-  Serial.print(" | Motor Speed L: ");
-  Serial.println(motorspeedR);
-  Serial.print(" | Kp: ");
-  Serial.print(KP,5);
-  Serial.print(" | Ki: ");
-  Serial.print(KI,5);
-  Serial.print(" | Kd: ");
-  Serial.print(KD,5);
-  Serial.print(" | ");
-
-    // Check for serial input
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
-    input.trim();
-
-    // Parse the input and update PID constants
-    if (input.startsWith("Kp")) {
-      KP = input.substring(3).toFloat();
-      
-      Serial.print("Kp updated to: ");
-      Serial.println(KP);
-    } else if (input.startsWith("Ki")) {
-      KI = input.substring(3).toFloat();
-     
-      Serial.print("Ki updated to: ");
-      Serial.println(KI);
-    } else if (input.startsWith("Kd")) {
-      KD = input.substring(3).toFloat();
-      Serial.print("Kd updated to: ");
-      Serial.println(KD);
-    }
-  }
-
-
-  delay(1); // Ajusta el retardo según sea necesario
 }
